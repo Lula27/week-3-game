128a465209681d111433dbb480dec0864cab3a91
Loretta Agyemang <Loretta Agyemang>
2016-11-18 12:58:40 -0500

    created main variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,22 @@
 
-/*Steps:  1. message alerting user welcome to game:  "Welcome to Mind Trix! Click Enter to begin"
-		2. user selection options= alphabets in keybord (make and store array of letters)
-		3. computer stores selected chosen letters (asdfghjkl) are winning letters
+/*Steps:  1. message alerting user welcome to game:  "Welcome to Mind Trix! Click Enter to begin"  + 
+		2. user selection options= alphabets in keybord (make and store array of letters)          +
+		3. computer stores selected chosen letters (asdfghjkl) are winning letters                 + 
 		4. user gets point with correct letter 
-		5. wins accumulated and saved in "Wins"
-		6. user gets points which are stored in "Lossess" section
+		5. wins accumulated and saved in "Wins"													   +
+		6. user gets points which are stored in "Losses" section 								   + 
 		6. provide running list of letters already selected with previous guesses
 		8. list of remaining guesses (user starts with 5 guesses and -1 with each lost guess) 
 		7. 3 wins and 3 losses restart game 
 		8. Provide optional "Play again" button to restart game */
+
+
 alert("Welcome to Mind Trix! Click OK to begin."); 
+
+var choice = 'abcdefghijklmnopqrstuvwxyz'.split(''),  winners ='asdfghjkl'.split(''); 
+console.log(choice, winners); 
+
+var wins = 0; 
+var losses = 0;
+var guessesLeft = 5; 
+var guessesSoFar = []; 
