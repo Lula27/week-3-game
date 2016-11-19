b4ecaa721a79285244284bc6f31b251fc5dfc4a0
Loretta Agyemang <Loretta Agyemang>
2016-11-19 07:50:20 -0500

    needs more work

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<!-- <!-- <?php include_once("C:/Users/loret/Desktop/RCB/W3_JavaScript/Homework3/week-3-game/index.html"); ?> <!--Is this correct?--> 
+<?php include_once("C:\Users\loret\Desktop\RCB\W3_JavaScript\Homework3\week-3-game/index.html"); ?> 
