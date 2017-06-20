a8c57e1d74db24ec861585ae007bce96781b3b64
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-06-19 23:04:09 -0400

    working on it again

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,1 +1,16 @@
-alert('Hi!'); 
+// Goal: use keyboard to guess what letters the computer is thinking
+// Use button to reset game
+
+var numbers, WinningNumbers, LossingNumbers;
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+WinningNumbers = [2, 4, 6, 8, 10];
+
+LossingNumbers = [3, 5, 7, 9];
+
+
+
+myFunction() {
+	document.getElementById("output").innerHTML = numbers; 
+}
