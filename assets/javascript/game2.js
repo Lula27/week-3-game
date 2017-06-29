4af4477baca1d55fa8a420ffe9bb383968401e1e
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-06-28 22:59:19 -0400

    succesfully got a response with an event

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,7 +2,7 @@
 
 // Step 1: Have computer print a key from the key board
 
-var letter = l; 
+// var letter = l; 
 
 // Function to retrive an action 
 // Just like you have to declare a variable, you declare a function!
@@ -10,7 +10,13 @@ var letter = l;
 // 	console.log("Hi!");
 // }
 
-function myFunction(p1, p2) {
-	return p1 * p2; 
-	console.log(return p1 * p2);
+// function myFunction(p1, p2) {
+// 	return p1 * p2; 
+// 	console.log(return p1 * p2);
+// }
+
+document.getElementById("demo").addEventListener("keydown", myFunction);
+
+function myFunction() {
+	document.getElementById("demo").style.backgroundColor = "red";
 }
