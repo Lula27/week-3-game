e6d15daac67bc20b01590c968b726d049c892e04
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-07-13 23:42:51 -0400

    style change

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -65,8 +65,8 @@ document.getElementById("game").addEventListener("click", addElement);
 function addElement () {
 	// Create new div element & git it some content
 	var newDiv = document.createElement("div");
-	var questionOne = document.createTextNode("Name me: '\"I was the conductor of the Underground Railroad for eight years, and I can say what most conductors can't say; I never ran my train off the track and I never lost a passenger.\"");
-	newDiv.appendChild(questionOne);
+	// var questionOne = document.createTextNode("Name me: '\"I was the conductor of the Underground Railroad for eight years, and I can say what most conductors can't say; I never ran my train off the track and I never lost a passenger.\"");
+	// newDiv.appendChild(questionOne);
 
 
 	// add newly created element and its content into the DOM
@@ -82,7 +82,7 @@ for (i = 0; i < 2; i++) {
 }
 
 // Practice with LoopS
-var text = "Name me: '\"I was the conductor of the Underground Railroad for eight years, and I can say what most conductors can't say; I never ran my train off the track and I never lost a passenger.\"";
+var text = ;
 var td; 
 for (i = 0; i < 2; i++) {
 	td = document.getElementById("game");
