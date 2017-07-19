3f812ee77bacda1919e87904755d7819b4210b5d
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-07-18 22:55:17 -0400

    working again - commented out everything

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,51 +1,3 @@
-// Goal: 
-
-// Game Rules
-// Hit Start -> Takes you to past, present or future randomly 
-
-// Step 1: Have computer print a key from the key board
-
-// var letter = l; 
-
-// Function to retrive an action 
-// Just like you have to declare a variable, you declare a function!
-// myFunction() {
-// 	console.log("Hi!");
-// }
-
-// function myFunction(p1, p2) {
-// 	return p1 * p2; 
-// 	console.log(return p1 * p2);
-// }
-
-// Outside of text box turns red when user presses key
-// document.getElementById("demo").addEventListener("keydown", myFunction);
-
-// function myFunction() {
-// 	document.getElementById("demo").style.backgroundColor = "red";
-// }
-
-
-
-// document.getElementById("demo2").addEventListener("keydown", anotherFunction);
-
-// function anotherFunction() {
-// 	alert("This is a working function!"); 
-// }
-
-
-
-
-// Third Box & Function 
-
-// document.getElementById("demo3").addEventListener("keydown", toCall); 
-
-// Function to cause text to appear
-
-// function toCall() {
-// 	document.getElementById("change").innerHTML = "Hello World!"; 
-// }
-
 
 // Game Rules
 // 1. User enters mother's name to start game (what if doesn't have mother/bad relationship?)
@@ -80,41 +32,37 @@ function addElement () {
 }
 
 // for loop to get text to stay
-for (i = 0; i < 2; i++) {
-	document.getElementById("game").addEventListener("click", addElement);
-}
-
-// Practice with LoopS
-// var text = "Name me: '\"I was the conductor of the Underground Railroad for eight years, and I can say what most conductors can't say; I never ran my train off the track and I never lost a passenger.\"";
-// var td; 
 // for (i = 0; i < 2; i++) {
-// 	td = document.getElementById("game");
-// 	if (typeof window.addEventListener === "click"){
-// 		(function(_td) {
-// 			td.addEventListener("click", function(){
-// 				console.log(text);
-// 			});
-// 		})(text); 
-// 	}
+// 	document.getElementById("game").addEventListener("click", addElement);
 // }
 
-// create variables for the different options 
-	var wrong1 = document.getElementById("truth");
-	var wrong2 = document.getElementById("yaa");
-	var wrong3 = document.getElementById("nzinga");
-	var right = document.getElementById("harriet");
+// // Practice with LoopS
+// // var text = "Name me: '\"I was the conductor of the Underground Railroad for eight years, and I can say what most conductors can't say; I never ran my train off the track and I never lost a passenger.\"";
+// // var td; 
+// // for (i = 0; i < 2; i++) {
+// // 	td = document.getElementById("game");
+// // 	if (typeof window.addEventListener === "click"){
+// // 		(function(_td) {
+// // 			td.addEventListener("click", function(){
+// // 				console.log(text);
+// // 			});
+// // 		})(text); 
+// // 	}
+// // }
 
-	// Select Correct 
-	if (right) {
-		alert("You got it!"); 
-	}
+// // create variables for the different options 
+// 	var wrong1 = document.getElementById("truth");
+// 	var wrong2 = document.getElementById("yaa");
+// 	var wrong3 = document.getElementById("nzinga");
+// 	var right = document.getElementById("harriet");
+
+// 	// Select Correct 
+// 	if (right) {
+// 		alert("You got it!"); 
+// 	}
 
 
-right.addEventListener("click",Response); 
 
-// Response for button 
-function Response () {
-
-	alert("Correct!"); 
-
-}
+// right.addEventListener("click", function() {
+// 	alert("Correct!"); 
+// }
